09831dbc3fdeee6b8b6d2765377dd5b46b26b94c
Pritish-Sinha <bpb1789@gmail.com>
2023-10-15 14:40:17 +0530

    Build #1

---
+++ Fertilizer_Recommender/app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+
+# Load the dataset
+data = pd.read_csv("..\datasets\Fertilizer Prediction.csv")  
+# Define a function to predict fertilizer
+def predict_fertilizer(temperature, humidity, moisture, soil_type, crop_type, nitrogen, potassium, phosphorous):
+    # Label encoding for categorical features
+    soil_type_enc = le_soil.transform([soil_type])[0]
+    crop_type_enc = le_crop.transform([crop_type])[0]
+    
+    # Create user input data
+    user_input = [[temperature, humidity, moisture, soil_type_enc, crop_type_enc, nitrogen, potassium, phosphorous]]
+    
+    # Predict the fertilizer
+    fertilizer_name = model.predict(user_input)
+    return fertilizer_name[0]
+
+# Label encoding for categorical features
+le_soil = LabelEncoder()
+le_crop = LabelEncoder()
+
+# Fit the LabelEncoders with your dataset
+le_soil.fit(data['Soil Type'])
+le_crop.fit(data['Crop Type'])
+
+# Load the trained model
+model = joblib.load("Fertilizer.pkl")
+
+# Create a Streamlit web app
+st.title("Fertilizer Recommender")
+
+# Input fields
+temperature = st.slider("Temperature", min_value=0, max_value=100)
+humidity = st.slider("Humidity", min_value=0, max_value=100)
+moisture = st.slider("Moisture", min_value=0, max_value=100)
+soil_type = st.selectbox("Soil Type", le_soil.classes_)
+crop_type = st.selectbox("Crop Type", le_crop.classes_)
+nitrogen = st.slider("Nitrogen Level", min_value=0, max_value=100)
+potassium = st.slider("Potassium Level", min_value=0, max_value=100)
+phosphorous = st.slider("Phosphorous Level", min_value=0, max_value=100)
+
+if st.button("Predict"):
+    result = predict_fertilizer(temperature, humidity, moisture, soil_type, crop_type, nitrogen, potassium, phosphorous)
+    st.success(f"Recommended Fertilizer: {result}")
